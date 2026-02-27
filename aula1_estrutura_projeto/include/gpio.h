ce5e4139d721e79450f43b29f49a94c7ce2dbff5
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-02-27 16:16:09 -0300

    Arquivos iniciais do projeto

---
+++ aula1_estrutura_projeto/include/gpio.h
@@ -0,0 +1,15 @@
+/*
+ * gpio.h
+ *
+ * Created: 27/02/2026 14:04:46
+ *  Author: Aluno
+ */ 
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+void GPIO_initialize();
+void GPIO_blinkLed();
+
+#endif /* GPIO_H_ */
---
+++ aula1_estrutura_projeto/include/usart.h
@@ -0,0 +1,15 @@
+/*
+ * usart.h
+ *
+ * Created: 27/02/2026 14:04:59
+ *  Author: Aluno
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+
+void USART_initialize();
+void USART_send();
+
+#endif /* USART_H_ */
