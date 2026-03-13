5f6ba6f839202663580542bcf2b62963bc95ad2a
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-03-13 16:27:47 -0300

    Incluido lógica de interrupção utilizando PCINT

--- aula3_interrupts/main.c
+++ aula3_interrupts/main.c
@@ -12,6 +12,8 @@
 #include "util/delay.h"
 
 int gLedFlag = 0;
+int gPD2State = 0;
+int gPD3State = 0;
 
 ISR(INT0_vect) {
 	gLedFlag = 1;
@@ -21,11 +23,47 @@ ISR(INT1_vect) {
 	gLedFlag = 1;
 }
 
-int main(void) {
-	DDRC = (1<<DDC0);				// configura pino PC0 como saída (LED)
+ISR(PCINT2_vect) {
+	
+	if((PIND & (1<<PIND2)) != 0) {
+		//Pino D2 está no nível lógico alto
+		if(gPD2State == 0) {
+			//Ocorreu uma transição no PD2
+			gLedFlag = 1;
+		}
+		gPD2State = 1;
+	} else {
+		//Pino D2 está no nível lógico baixo
+		gPD2State = 0;
+	}
+	
+	if((PIND & (1<<PIND3)) != 0) {
+		//Pino D3 está no nível lógico alto
+		if(gPD3State == 0) {
+			//Ocorreu uma transição no PD3
+			gLedFlag = 1;
+		}
+		gPD3State = 1;
+	} else {
+		//Pino D3 está no nível lógico baixo
+		gPD3State = 0;
+	}
+}
+
+void EXTINT_config() {
 	EICRA = (0<<ISC01)|(1<<ISC00)	// INT0: Ambas as bordas
 		  | (1<<ISC11)|(1<<ISC10);	// INT1: Na borda de subida
-	EIMSK = (1<<INT1)|(1<<INT0);	// INT0 e INT1 habilitadas
+	EIMSK = (1<<INT1)|(1<<INT0);	// INT0 e INT1 habilitadas	
+}
+
+void PCINT_config() {
+	PCMSK2 |= (1<<PCINT18)|(1<<PCINT19);	// Habilita PCINT18 e PCINT19 (equivale pinos PD2 e PD3)
+	PCICR |= (1<<PCIE2);					// Habilita grupo 2 (equivale porta D)
+}
+
+int main(void) {
+	DDRC = (1<<DDC0);				// Configura pino PC0 como saída (LED)
+	PCINT_config(); //se quisermos usar INT0/1, trocar para EXTINT_config
 	sei();
     while(1) {
         if(gLedFlag) {
