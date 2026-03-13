b50b3f14e9cfb45307460569bbb761ecfa9564c4
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-03-13 15:13:15 -0300

    inclusão da interrupção INT1 na aula 3 também foi adicionado um flag para tratamento do LED no loop principal

--- aula3_interrupts/main.c
+++ aula3_interrupts/main.c
@@ -11,18 +11,28 @@
 #include "avr/interrupt.h"
 #include "util/delay.h"
 
+int gLedFlag = 0;
+
 ISR(INT0_vect) {
-	PORTC = (1<<PORTC0); //Alterna LED
-	_delay_ms(100);
-	PORTC = 0;
+	gLedFlag = 1;
+}
+
+ISR(INT1_vect) {
+	gLedFlag = 1;
 }
 
 int main(void) {
 	DDRC = (1<<DDC0);				// configura pino PC0 como saída (LED)
-	EICRA = (0<<ISC01)|(1<<ISC00);	// INT0: Ambas as bordas
-	EIMSK = (0<<INT1)|(1<<INT0);	// INT0 habilitada, INT1 desabilitada
+	EICRA = (0<<ISC01)|(1<<ISC00)	// INT0: Ambas as bordas
+		  | (1<<ISC11)|(1<<ISC10);	// INT1: Na borda de subida
+	EIMSK = (1<<INT1)|(1<<INT0);	// INT0 e INT1 habilitadas
 	sei();
     while(1) {
-        //TODO:: Please write your application code 
+        if(gLedFlag) {
+	        PORTC = (1<<PORTC0);
+	        _delay_ms(100);
+	        PORTC = 0;
+	        gLedFlag = 0;
+        }
     }
 }
